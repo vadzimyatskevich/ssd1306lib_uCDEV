2a07ba969c5f57d25975623314d9ad9da75c9df6
vadzimyatskevich <vadzimyatskevich@gmail.com>
2016-05-29 17:12:33 +0300

    fonts added

---
+++ Inc/ubuntuMono_16pt.h
@@ -0,0 +1,6 @@
+#include "stdint.h"
+#include "font.h"
+/* Font data for Ubuntu Mono 16pt */
+extern const uint8_t ubuntuMono_16ptBitmaps[];
+extern const FONT_INFO ubuntuMono_16ptFontInfo;
+extern const FONT_CHAR_INFO ubuntuMono_16ptDescriptors[];
---
+++ Inc/ubuntuMono_48pt.h
@@ -0,0 +1,6 @@
+#include "stdint.h"
+#include "font.h"
+/* Font data for Ubuntu Mono 48pt */
+extern const uint8_t ubuntuMono_48ptBitmaps[];
+extern const FONT_INFO ubuntuMono_48ptFontInfo;
+extern const FONT_CHAR_INFO ubuntuMono_48ptDescriptors[];
